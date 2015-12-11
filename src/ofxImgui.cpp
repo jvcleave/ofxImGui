1f89a34a8c2e6512ebb25684095494652315d104
Constantine Tarasenkov <constantine@protonmail.com>
2015-12-12 00:51:35 +0300

    Working version for GL 2.1

--- src/ofxImgui.cpp
+++ src/ofxImgui.cpp
@@ -182,6 +182,10 @@ void ofxImgui::renderDrawLists(ImDrawData * draw_data)
   glDisable(GL_DEPTH_TEST);
   glEnable(GL_SCISSOR_TEST);
 
+  glEnableClientState(GL_VERTEX_ARRAY);
+  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+  glEnableClientState(GL_COLOR_ARRAY);
+
   ImGuiIO& io = ImGui::GetIO();
   float fb_height = io.DisplaySize.y * io.DisplayFramebufferScale.y;
   draw_data->ScaleClipRects(io.DisplayFramebufferScale);
@@ -190,39 +194,75 @@ void ofxImgui::renderDrawLists(ImDrawData * draw_data)
   {
     const ImDrawList * cmd_list = draw_data->CmdLists[n];
 
-    ofMesh mesh;
+//    ofMesh mesh;
 
-    for(size_t i = 0; i < cmd_list->VtxBuffer.size(); i++)
-    {
-      mesh.addVertex(ofVec3f(cmd_list->VtxBuffer[i].pos.x, cmd_list->VtxBuffer[i].pos.y, 0.f));
-      mesh.addTexCoord(ofVec2f(cmd_list->VtxBuffer[i].uv.x, cmd_list->VtxBuffer[i].uv.y));
-      ImVec4 col = ImColor(cmd_list->VtxBuffer[i].col).Value;
-      mesh.addColor(ofFloatColor(col.x, col.y, col.z, col.w));
-    }
+//    for(size_t i = 0; i < cmd_list->VtxBuffer.size(); i++)
+//    {
+//      mesh.addVertex(ofVec3f(cmd_list->VtxBuffer[i].pos.x, cmd_list->VtxBuffer[i].pos.y, 0.f));
+//      mesh.addTexCoord(ofVec2f(cmd_list->VtxBuffer[i].uv.x, cmd_list->VtxBuffer[i].uv.y));
+//      ImVec4 col = ImColor(cmd_list->VtxBuffer[i].col).Value;
+//      mesh.addColor(ofFloatColor(col.x, col.y, col.z, col.w));
+//    }
 
-    for(size_t i = 0; i < cmd_list->IdxBuffer.size(); i++)
-    {
-      mesh.addIndex((ofIndexType)cmd_list->IdxBuffer[i]);
-    }
+//    for(size_t i = 0; i < cmd_list->IdxBuffer.size(); i++)
+//    {
+//      mesh.addIndex((ofIndexType)cmd_list->IdxBuffer[i]);
+//    }
 
-    for (const ImDrawCmd * pcmd = cmd_list->CmdBuffer.begin(); pcmd != cmd_list->CmdBuffer.end(); pcmd++)
+//    for (const ImDrawCmd * pcmd = cmd_list->CmdBuffer.begin(); pcmd != cmd_list->CmdBuffer.end(); pcmd++)
+//    {
+//      if (pcmd->UserCallback)
+//      {
+//          pcmd->UserCallback(cmd_list, pcmd);
+//      }
+//      else
+//      {
+//          ofTexture tex;
+//          tex.texData.textureTarget = GL_TEXTURE_2D;
+//          tex.setUseExternalTextureID((intptr_t)pcmd->TextureId);
+//          tex.bind();
+//          glScissor((int)pcmd->ClipRect.x, (int)(fb_height - pcmd->ClipRect.w), (int)(pcmd->ClipRect.z - pcmd->ClipRect.x), (int)(pcmd->ClipRect.w - pcmd->ClipRect.y));
+//          mesh.draw();
+//          tex.unbind();
+//      }
+//    }
+
+    #define OFFSETOF(TYPE, ELEMENT) ((size_t)&(((TYPE *)0)->ELEMENT))
+    for (int n = 0; n < draw_data->CmdListsCount; n++)
     {
-      if (pcmd->UserCallback)
-      {
-          pcmd->UserCallback(cmd_list, pcmd);
-      }
-      else
-      {
-          ofTexture tex;
-          tex.texData.textureTarget = GL_TEXTURE_2D;
-          tex.setUseExternalTextureID((intptr_t)pcmd->TextureId);
-          tex.bind();
-          glScissor((int)pcmd->ClipRect.x, (int)(fb_height - pcmd->ClipRect.w), (int)(pcmd->ClipRect.z - pcmd->ClipRect.x), (int)(pcmd->ClipRect.w - pcmd->ClipRect.y));
-          mesh.draw();
-          tex.unbind();
-      }
+        const ImDrawList* cmd_list = draw_data->CmdLists[n];
+        const unsigned char* vtx_buffer = (const unsigned char*)&cmd_list->VtxBuffer.front();
+        const ImDrawIdx* idx_buffer = &cmd_list->IdxBuffer.front();
+        glVertexPointer(2, GL_FLOAT, sizeof(ImDrawVert), (void*)(vtx_buffer + OFFSETOF(ImDrawVert, pos)));
+        glTexCoordPointer(2, GL_FLOAT, sizeof(ImDrawVert), (void*)(vtx_buffer + OFFSETOF(ImDrawVert, uv)));
+        glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(ImDrawVert), (void*)(vtx_buffer + OFFSETOF(ImDrawVert, col)));
+
+        for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.size(); cmd_i++)
+        {
+            const ImDrawCmd* pcmd = &cmd_list->CmdBuffer[cmd_i];
+            if (pcmd->UserCallback)
+            {
+                pcmd->UserCallback(cmd_list, pcmd);
+            }
+            else
+            {
+                ofTexture tex;
+                tex.texData.textureTarget = GL_TEXTURE_2D;
+                tex.setUseExternalTextureID((intptr_t)pcmd->TextureId);
+                tex.bind();
+                glScissor((int)pcmd->ClipRect.x, (int)(fb_height - pcmd->ClipRect.w), (int)(pcmd->ClipRect.z - pcmd->ClipRect.x), (int)(pcmd->ClipRect.w - pcmd->ClipRect.y));
+                glDrawElements(GL_TRIANGLES, (GLsizei)pcmd->ElemCount, sizeof(ImDrawIdx) == 2 ? GL_UNSIGNED_SHORT : GL_UNSIGNED_INT, idx_buffer);
+                tex.unbind();
+            }
+            idx_buffer += pcmd->ElemCount;
+        }
     }
+    #undef OFFSETOF
   }
+
+  glDisableClientState(GL_COLOR_ARRAY);
+  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+  glDisableClientState(GL_VERTEX_ARRAY);
 
   glBlendEquationSeparate(last_blend_equation_rgb, last_blend_equation_alpha);
   glBlendFunc(last_blend_src, last_blend_dst);
