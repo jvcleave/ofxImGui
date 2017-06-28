c1a0b20e33f87d5e66871a3c0a7f585d09beb45a
Tim Gfrerer <tim@poniesandlight.co.uk>
2017-06-28 19:37:35 +0100

    extract ofxImGuiLoggerChannel

---
+++ src/ofxImGuiLoggerChannel.h
@@ -0,0 +1,24 @@
+#ifndef GUARD_IM_GUI_LOGGER_CHANNEL
+#define GUARD_IM_GUI_LOGGER_CHANNEL
+#include "imgui.h"
+#include "ofLog.h"
+
+namespace ofxImGui {
+
+/// \brief A logger channel that logs its messages to the console.
+class LoggerChannel : public ofBaseLoggerChannel
+{
+public:
+
+	static ImGuiTextBuffer & getBuffer();
+
+	/// \brief Destroy the console logger channel.
+	virtual ~LoggerChannel(){};
+	void log( ofLogLevel level, const string & module, const string & message );
+	void log( ofLogLevel level, const string & module, const char* format, ... ) OF_PRINTF_ATTR( 4, 5 );
+	void log( ofLogLevel level, const string & module, const char* format, va_list args );
+};
+
+} // end namespace ofxImGui
+
+#endif // IM_GUI_LOGGER_CHANNEL
