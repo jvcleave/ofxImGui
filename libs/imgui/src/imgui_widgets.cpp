889d0c73fdc49d19b5be8c4a91003190f2631043
Constant Dupuis <constant.dupuis@gmail.com>
2019-09-30 15:49:18 +0200

    Fix Knob needle trail gone

--- libs/imgui/src/imgui_widgets.cpp
+++ libs/imgui/src/imgui_widgets.cpp
@@ -1164,15 +1164,14 @@ void ImGui::Bullet()
     SameLine(0, style.FramePadding.x * 2.0f);
 }
 
-bool ImGui::Knob(const char* label, float* p_value, float v_min, float v_max, float radius, int valuePrecision)
+bool ImGui::Knob(const char* label, float* p_value, float v_min, float v_max, float radius, bool needleTrail, float incPrecision)
 {
 	ImGuiIO& io = ImGui::GetIO();
 	ImGuiStyle& style = ImGui::GetStyle();
 
-	float radius_outer = radius;
-	ImVec2 pos = ImGui::GetCursorScreenPos(); // get top left of current widget
+	ImVec2 pos = ImGui::GetCursorScreenPos(); // get top left pos of current widget
 	float line_height = ImGui::GetTextLineHeight();
-	float space_height = line_height * 0.1; // to add between top, text, knob, value and bottom
+	float space_height = line_height * 0.1; // to add between top, label, knob, value and bottom
 	float space_width = radius * 0.1; // to add on left and right to diameter of knob
 	ImVec4 widgetRec = ImVec4(pos.x, pos.y, radius*2.0f + space_width * 2.0f, space_height*4.0f + radius * 2.0f + line_height * 2.0f);
 	ImVec2 labelLength = ImGui::CalcTextSize(label);
@@ -1187,10 +1186,10 @@ bool ImGui::Knob(const char* label, float* p_value, float v_min, float v_max, fl
 	ImGui::InvisibleButton(label, ImVec2(widgetRec.z, widgetRec.w));
 	bool value_changed = false;
 	bool is_active = ImGui::IsItemActive();
-	bool is_hovered = ImGui::IsItemActive();
+	bool is_hovered = ImGui::IsItemHovered();
 	if (is_active && io.MouseDelta.x != 0.0f)
 	{
-		float step = (v_max - v_min) / valuePrecision;
+		float step = (v_max - v_min) / incPrecision;
 		*p_value += io.MouseDelta.x * step;
 		if (*p_value < v_min) *p_value = v_min;
 		if (*p_value > v_max) *p_value = v_max;
@@ -1200,15 +1199,25 @@ bool ImGui::Knob(const char* label, float* p_value, float v_min, float v_max, fl
 	float t = (*p_value - v_min) / (v_max - v_min);
 	float angle = ANGLE_MIN + (ANGLE_MAX - ANGLE_MIN) * t;
 	float angle_cos = cosf(angle), angle_sin = sinf(angle);
-	float radius_inner = radius_outer * 0.40f;
+	float radius_inner = radius * 0.35f;
 
 	//draw label
 	float texPos = pos.x + ((widgetRec.z - labelLength.x) * 0.5f);
 	draw_list->AddText(ImVec2(texPos, pos.y + space_height), ImGui::GetColorU32(ImGuiCol_Text), label);
 	// draw knob
-	draw_list->AddCircleFilled(center, radius_outer, ImGui::GetColorU32(ImGuiCol_FrameBg), 16);
-	draw_list->AddLine(ImVec2(center.x + angle_cos * radius_inner, center.y + angle_sin * radius_inner), ImVec2(center.x + angle_cos * (radius_outer - 2), center.y + angle_sin * (radius_outer - 2)), ImGui::GetColorU32(ImGuiCol_SliderGrabActive), 2.0f);
-	draw_list->AddCircleFilled(center, radius_inner, ImGui::GetColorU32(is_active ? ImGuiCol_FrameBgActive : is_hovered ? ImGuiCol_FrameBgHovered : ImGuiCol_FrameBg), 16);
+	draw_list->AddCircleFilled(center, radius, ImGui::GetColorU32(ImGuiCol_FrameBg), 16); // draw outer knob
+	draw_list->AddLine(ImVec2(center.x + angle_cos * radius_inner, center.y + angle_sin * radius_inner),
+		ImVec2(center.x + angle_cos * (radius), center.y + angle_sin * (radius)),
+		ImGui::GetColorU32(ImGuiCol_SliderGrabActive), 2.0f); // draw needle
+
+	if (needleTrail) // draw needle trail ?
+	{
+		draw_list->PathArcTo(center, radius - 2.8f, ANGLE_MIN, angle, 2 + (4 * (angle - ANGLE_MIN))); // draw needle trail
+		draw_list->PathStroke(ImGui::GetColorU32(ImGuiCol_SliderGrabActive), false, 6.0f);
+	}
+
+	//draw_list->AddCircleFilled(center, radius_inner, ImGui::GetColorU32(is_active ? ImGuiCol_FrameBgActive : is_hovered ? ImGuiCol_FrameBgHovered : ImGuiCol_FrameBg), 16); // draw inner knob
+	draw_list->AddCircleFilled(center, radius_inner, ImGui::GetColorU32(is_active ? ImGuiCol_FrameBgActive : is_hovered ? ImGuiCol_SliderGrabActive : ImGuiCol_FrameBg), 16); // draw inner knob
 
 	// draw value
 	char temp_buf[64];
