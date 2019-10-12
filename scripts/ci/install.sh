316a88416a3d561017ea5bfd17d901383201ba83
jason van cleave <jvcleave@gmail.com>
2019-10-12 11:51:15 -0600

    ci

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
- if [ "$TARGET" != "ios" ]; then
- cd $TRAVIS_BUILD_DIR
-  rm -Rf /home/travis/openFrameworks/addons/ofxImGui/example-ios
- fi
- 
+
+rm -Rf /home/travis/openFrameworks/addons/ofxImGui/example-ios
