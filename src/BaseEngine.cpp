807f03a1419ae6874f3a83f9bef4ba5011c6f84e
d3cod3 <n3m3da@d3cod3.org>
2019-12-09 08:45:07 +0100

    fixed paste from clipboard

--- src/BaseEngine.cpp
+++ src/BaseEngine.cpp
@@ -71,7 +71,10 @@ namespace ofxImGui
 	//--------------------------------------------------------------
 	const char* BaseEngine::getClipboardString(void * userData)
 	{
-		return &ofGetWindowPtr()->getClipboardString()[0];
+        //return &ofGetWindowPtr()->getClipboardString()[0];
+        static std::string clip;
+        clip = ofGetWindowPtr()->getClipboardString();
+        return clip.c_str();
 	}
 
 	//--------------------------------------------------------------
