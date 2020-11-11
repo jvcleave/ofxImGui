5a657fbe06008ba2babbad7accfac10fa38b7eb4
Daan <idaany@gmail.com>
2020-11-11 14:30:03 +0100

    Remove unnecessary example files (they are generated by the PG)

--- example-demo/fixDynamicLibs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-APPNAME="example-demo_debug"
-
-install_name_tool -change @rpath/libndi.3.dylib @executable_path/libndi.3.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
