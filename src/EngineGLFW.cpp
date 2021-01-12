ae20f9dc4fb07dd6519727e41974ad45d42b6384
Daan <idaany@gmail.com>
2021-01-12 16:18:07 +0100

    Fix OFXIMGUI_ENABLE_OF_BINDINGS for engineGLFW

--- src/EngineGLFW.cpp
+++ src/EngineGLFW.cpp
@@ -261,11 +261,11 @@ namespace ofxImGui
         //ImGui::Render(); // also calls ImGui::EndFrame()
 
         if (ofIsGLProgrammableRenderer()) {
-            int display_w, display_h;
-            glfwGetFramebufferSize(window, &display_w, &display_h);
-            glViewport(0, 0, display_w, display_h);
-            glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-            glClear(GL_COLOR_BUFFER_BIT);
+//            int display_w, display_h;
+//            glfwGetFramebufferSize(window, &display_w, &display_h);
+//            glViewport(0, 0, display_w, display_h);
+//            glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+//            glClear(GL_COLOR_BUFFER_BIT);
             programmableDrawData(ImGui::GetDrawData());
         }
         else {
