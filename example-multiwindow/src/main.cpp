ea738805c1243078c087c8004c70680dfe95bfab
Daan <idaany@gmail.com>
2021-01-13 17:10:42 +0100

    Reviewed changes to multicontext example. Removed multiwindow features from context example.

--- example-multiwindow/src/main.cpp
+++ example-multiwindow/src/main.cpp
@@ -4,19 +4,16 @@
 
 //========================================================================
 int main( ){
-	ofGLFWWindowSettings settings;
-	settings.setSize(300, 300);
-	settings.setPosition(ofVec2f(0, 100));
-	auto window1 = ofCreateWindow(settings);
+    ofGLFWWindowSettings settings;
+    //settings.setSize(300, 300);
+    //settings.setPosition(ofVec2f(0, 100));
+    //settings.setGLVersion(3,2);
+    settings.title="ofxImGui MultiContext Demo";
+    auto window1 = ofCreateWindow(settings);
 
-	settings.setPosition(ofVec2f(300, 100));
-	auto window2 = ofCreateWindow(settings);
+    auto app1 = std::make_shared<ofApp>();
 
-	auto app1 = std::make_shared<ofApp>();
-	auto app2 = std::make_shared<ofApp>();
-
-	ofRunApp(window1, app1);
-	ofRunApp(window2, app2);
+    ofRunApp(window1, app1);
 
 	ofRunMainLoop();
 }
--- example-multiwindow/src/ofApp.h
+++ example-multiwindow/src/ofApp.h
@@ -3,6 +3,9 @@
 #include "ofMain.h"
 #include "ofxImGui.h"
 
+// Uncomment to use a slightly different method
+//#define ALTERNATIVE_IMPLEMENTATION
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -11,27 +14,60 @@ class ofApp : public ofBaseApp{
 			v2 = 0;
 			v1 = 0;
 
-			gui.setup(false, true); // No autodraw, Allow chaining
+            gui1.setup(nullptr, false);
+            //gui1.setup(nullptr, true); // Note: it's also possible to use autodraw
+
+            #ifdef ALTERNATIVE_IMPLEMENTATION
+            gui1.forceSharedMode(true); // Force share context
+            #endif
+
+            { // scope = called plugin code that you can't change
+
+                #ifndef ALTERNATIVE_IMPLEMENTATION
+                gui2.setup();
+
+                #else
+                gui2 = &gui1;
+                #endif
+            }
 		}
 
 		void draw() {
 			// [...] some code
-			gui.begin(); //   <-- First call = Normal behaviour
+            gui1.begin(); //   <-- First call = Normal behaviour
 			ImGui::SliderFloat("slider1", &v1, 0.f, 10.f); //....
 			ImGui::SliderFloat("slider2", &v2, 0.f, 10.f); //....
-			gui.end(); //     <-- Does nothing with chaining on
-			// [...] more code
-			{ // scope = called plugin code that you can't change
-				gui.begin(); // <-- Does nothing with chaining on
-				ImGui::SliderFloat("slider1", &v1, 0.f, 10.f); //....
-				ImGui::SliderFloat("slider2", &v2, 0.f, 10.f); //....
-				gui.end(); //   <-- Does nothing with chaining on
+            gui1.end(); //     <-- Does nothing with sharedMode on
+
+            ImGui::PushID("###Sandbox"); // <-- if you want to ensure a sandbox between both
+
+            { // scope = called plugin code that you can't change
+
+                #ifndef ALTERNATIVE_IMPLEMENTATION
+                gui2.begin(); // <-- Does nothing with sharedMode on
+                ImGui::SliderFloat("slider1", &v3, 0.f, 10.f); //....
+                ImGui::SliderFloat("slider2", &v4, 0.f, 10.f); //....
+                gui2.end(); //   <-- Does nothing with sharedMode on
+                #else
+
+                gui2->begin(); // <-- Does nothing with sharedMode on
+                ImGui::SliderFloat("slider1", &v3, 0.f, 10.f); //....
+                ImGui::SliderFloat("slider2", &v4, 0.f, 10.f); //....
+                gui2->end(); //   <-- Does nothing with sharedMode on
+                #endif
 			}
-			gui.draw(); //    <-- Also calls gui.end()
+            ImGui::PopID(); // <-- If you want to ensure a sandbox between both
+
+            gui1.draw(); //    <-- Also calls gui.end()
 		}
 
+
 	private:
-		ofxImGui::Gui gui;
-		float v1;
-		float v2;
+        ofxImGui::Gui gui1;
+#ifdef ALTERNATIVE_IMPLEMENTATION
+        ofxImGui::Gui* gui2;
+#else
+        ofxImGui::Gui gui2;
+#endif
+        float v1, v2, v3, v4;
 };
