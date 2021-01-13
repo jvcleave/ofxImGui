d2a95b7b9e7f68504c9d668ff0ae668f418e768e
Daan <idaany@gmail.com>
2021-01-13 18:00:08 +0100

    Updated multiwindow example (still some bugs around)

--- example-multiwindow/src/main.cpp
+++ example-multiwindow/src/main.cpp
@@ -2,21 +2,37 @@
 #include "ofApp.h"
 #include "ofAppGLFWWindow.h"
 
+// Demo define
+#define USE_GL3
+
 //========================================================================
 int main( ){
+
 	ofGLFWWindowSettings settings;
-	settings.setSize(300, 300);
+    settings.setSize(300, 400);
 	settings.setPosition(ofVec2f(0, 100));
+#ifdef USE_GL3
+    settings.setGLVersion(3, 2);
+#else
+    settings.setGLVersion(2, 1);
+#endif
+    settings.title="Window 1";
 	auto window1 = ofCreateWindow(settings);
 
 	settings.setPosition(ofVec2f(300, 100));
-	auto window2 = ofCreateWindow(settings);
+    settings.setSize(500, 600);
+    settings.title="Window 2";
+    settings.shareContextWith = window1;
+    // WARNING : settings.setGLVersion(2, 1);
+    // For now, both GL versions must be the same for ImGui to work in a multiwindow context.
+    auto window2 = ofCreateWindow(settings);
+    std::cout << "Window 1 = " << window1.get() << " // " << (ofAppGLFWWindow*) window1.get() << std::endl;
+    std::cout << "Window 2 = " << window2.get() << " // " << (ofAppGLFWWindow*) window2.get() << std::endl;
+    auto app1 = std::make_shared<ofApp>();
+    auto app2 = std::make_shared<ofApp>();
 
-	auto app1 = std::make_shared<ofApp>();
-	auto app2 = std::make_shared<ofApp>();
-
-	ofRunApp(window1, app1);
-	ofRunApp(window2, app2);
+    ofRunApp(window1, app1);
+    ofRunApp(window2, app2);
 
 	ofRunMainLoop();
 }
--- example-multiwindow/src/ofApp.h
+++ example-multiwindow/src/ofApp.h
@@ -9,12 +9,26 @@ class ofApp : public ofBaseApp{
 		ofApp() : v(0) {}
 
 		void setup() {
-			gui.setup();
+            gui.setup();
 		}
 		void draw() {
+            // Warning message
+            ofDrawBitmapStringHighlight("Known bugs :", 0, ofGetHeight()-45);
+            ofDrawBitmapStringHighlight("Both windows have to be the same height, otherwise there's a mouse offset !", 0, ofGetHeight()-25);
+            ofDrawBitmapStringHighlight("Also, clipping occurs when their sizes differ.", 0, ofGetHeight()-10);
+
+            // Draw to ImGui (after OF related stuff, to make it overlay)
 			gui.begin();
+
+            ImGui::Begin("MultiWindow");
 			ImGui::SliderFloat("slider", &v, 0.f, 10.f);
+            ImGui::End();
+
+            ImGui::ShowMetricsWindow();
+            //ImGui::ShowDemoWindow();
+
 			gui.end();
+            gui.draw();
 		}
 
 	private:
