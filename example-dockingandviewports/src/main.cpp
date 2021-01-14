33cee2788cf605b9aa9219877e5d22b28cfeef27
Daan <idaany@gmail.com>
2021-01-14 20:05:51 +0100

    Added a docking and viewports example

---
+++ example-dockingandviewports/src/main.cpp
@@ -0,0 +1,15 @@
+#include "ofMain.h"
+#include "ofApp.h"
+#include "ofAppGLFWWindow.h"
+
+//========================================================================
+int main( ){
+    ofGLFWWindowSettings settings;
+    settings.title="ofxImGui Example : Docking and Viewports";
+
+	auto window1 = ofCreateWindow(settings);
+	auto app1 = std::make_shared<ofApp>();
+
+    ofRunApp(window1, app1);
+	ofRunMainLoop();
+}
---
+++ example-dockingandviewports/src/ofApp.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxImGui.h"
+#include "imgui_internal.h" // <-- example uses some imgui internals...
+
+class ofApp : public ofBaseApp{
+
+	public:
+        ofApp() {}
+
+		void setup() {
+            // Setup imgui with the appropriate config flags
+            gui.setup(nullptr, false, ImGuiConfigFlags_DockingEnable | ImGuiConfigFlags_ViewportsEnable, true);
+
+            // Uncomment below to perform docking with SHIFT key
+            // Gives a better user experience, matter of opinion.
+            //ImGui::GetIO().ConfigDockingWithShift=true;
+
+            ofSetBackgroundAuto(false);
+            ofEnableAlphaBlending();
+            ofSetRectMode(OF_RECTMODE_CENTER);
+
+        }
+
+		void draw() {
+            // erase the bg progressively to reveal which ImGui window are within the OF window
+            ofPushStyle();
+            ofEnableBlendMode(OF_BLENDMODE_SUBTRACT);
+            ofFill();
+            ofSetColor(200, 200, 200, 2);
+            ofDrawRectangle(ofGetWidth()*.5f, ofGetHeight()*.5f, ofGetWidth()+20, ofGetHeight()+20);
+            ofPopStyle();
+
+            ofPushStyle();
+            // Example info text
+            ofDrawBitmapStringHighlight(
+                        "Docking windows lets you easily re-arrange your window space.\n"\
+                        "Start dragging a window and drop it on a dockable zone !\n\n"\
+
+                        "Viewports : When you drag a window out of the oF window,\n"\
+                        "it seamlessly leaves the oF window context to become standalone.\n"\
+                        "if you see onion skins while dragging the window, it's in the same window as oF.",
+                        100, 40
+                        );
+            ofPopStyle();
+
+
+            // Start drawing to ImGui (newFrame)
+			gui.begin();
+
+            // Define the ofWindow as a docking space
+            ImGuiID dockNodeID = ImGui::DockSpaceOverViewport(NULL, ImGuiDockNodeFlags_PassthruCentralNode);
+
+            ImGuiDockNode* dockNode = ImGui::DockBuilderGetNode(dockNodeID);
+            if(dockNode){
+                // Verifies if the central node is empty (visible empty space for oF)
+                if(ImGui::DockBuilderGetCentralNode(dockNodeID)->IsEmpty()){
+                    ImRect availableSpace = ImGui::DockBuilderGetCentralNode(dockNodeID)->Rect();
+                    ImGui::GetForegroundDrawList()->AddRect(availableSpace.GetTL()+ImVec2(8,8), availableSpace.GetBR()-ImVec2(8,8), IM_COL32(255,50,50,255));
+
+                    ImVec2 viewCenter = availableSpace.GetCenter();
+                    // Depending on the viewports flag, the XY is either absolute or relative to the oF window.
+                    if(ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable ) viewCenter = viewCenter - ImVec2(ofGetWindowPositionX(),ofGetWindowPositionY());
+
+                    ofPushStyle();
+                    ofSetColor(255,0,0,2);
+                    ofNoFill();
+
+                    ofDrawRectangle(
+                                viewCenter.x,
+                                viewCenter.y,
+                                availableSpace.GetSize().x-6,
+                                availableSpace.GetSize().y-6
+                    );
+                    ofNoFill();
+                    ofSetColor(255,255,255,30);
+                    ofDrawRectangle(
+                                (viewCenter.x),
+                                (viewCenter.y),
+                                (availableSpace.GetSize().x-20)*fmodf(abs(sin(ofGetElapsedTimef())),1.f),
+                                (availableSpace.GetSize().y-20)*fmodf(abs(sin(ofGetElapsedTimef())),1.f)
+                    );
+                    ofPopStyle();
+                }
+            }
+
+            // Draw a few windows
+            // ImGuiWindowFlags_DockNodeHost => prevents hosting other windows, but still can be docked into another one.
+            drawWindow("Dockable Tabs",     50,     100, ImGuiWindowFlags_None          );
+            drawWindow("Window 1",          300,    100, ImGuiWindowFlags_DockNodeHost  );
+            drawWindow("Window 2",          50,     350, ImGuiWindowFlags_DockNodeHost  );
+            drawWindow("Window 3",          300,    350, ImGuiWindowFlags_DockNodeHost  );
+            drawWindow("Window 4",          550,    100, ImGuiWindowFlags_DockNodeHost  );
+            drawWindow("Window 5",          550,    350, ImGuiWindowFlags_DockNodeHost  );
+
+            // End our ImGui Frame. Also Renders in autoDraw mode.
+            gui.end();
+
+            gui.draw(); // <-- In manual mode, you can choose to render imgui at a given moment in your pipeline
+
+            // Show FPS
+            ofDrawBitmapStringHighlight( ofToString(ofGetFrameRate() ), 10, 50, ofColor(0,60), ofColor(255,128)); // <-- This text will be drawn over the layout
+		}
+
+    private:
+
+        void drawWindow(const char* _title, int _x=300, int _y=300, ImGuiWindowFlags _flags=ImGuiWindowFlags_None ){
+            ImGui::SetNextWindowSize(ImVec2(200,200), ImGuiCond_Once);
+            ImGui::SetNextWindowPos(ImVec2(_x+ofGetWindowPositionX(),_y+ofGetWindowPositionY()), ImGuiCond_Once);
+            ImGui::Begin(_title, NULL, _flags );
+            static int v = 0;
+            ImGui::InputInt("InputInt", &v);
+            ImGui::SliderInt("SliderInt", &v, 0, 10);
+            ImGui::DragInt("DragInt", &v);
+            ImGui::End();
+        }
+
+        ofxImGui::Gui gui;
+};
