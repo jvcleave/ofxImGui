fdbe58add71814d5ae09445077c2cb661504085c
Daan <idaany@gmail.com>
2021-06-28 13:23:28 +0200

    Forgot this

--- libs/imgui/backends/imgui_impl_glfw.cpp
+++ libs/imgui/backends/imgui_impl_glfw.cpp
@@ -72,76 +72,8 @@
 #define GLFW_HAS_MOUSE_PASSTHROUGH    (0)
 #endif
 
-// Dirty: Add support for older GLFW releases (needed for RPi Stretch at least, Glfw 3.2)
-#ifndef GLFW_GAMEPAD_BUTTON_A
-#define GLFW_GAMEPAD_BUTTON_A               0
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_B
-#define GLFW_GAMEPAD_BUTTON_B               1
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_X
-#define GLFW_GAMEPAD_BUTTON_X               2
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_Y
-#define GLFW_GAMEPAD_BUTTON_Y               3
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_LEFT_BUMPER
-#define GLFW_GAMEPAD_BUTTON_LEFT_BUMPER     4
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER
-#define GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER    5
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_BACK
-#define GLFW_GAMEPAD_BUTTON_BACK            6
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_START
-#define GLFW_GAMEPAD_BUTTON_START           7
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_GUIDE
-#define GLFW_GAMEPAD_BUTTON_GUIDE           8
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_LEFT_THUMB
-#define GLFW_GAMEPAD_BUTTON_LEFT_THUMB      9
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_RIGHT_THUMB
-#define GLFW_GAMEPAD_BUTTON_RIGHT_THUMB     10
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_DPAD_UP
-#define GLFW_GAMEPAD_BUTTON_DPAD_UP         11
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_DPAD_RIGHT
-#define GLFW_GAMEPAD_BUTTON_DPAD_RIGHT      12
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_DPAD_DOWN
-#define GLFW_GAMEPAD_BUTTON_DPAD_DOWN       13
-#endif
-#ifndef GLFW_GAMEPAD_BUTTON_DPAD_LEFT
-#define GLFW_GAMEPAD_BUTTON_DPAD_LEFT       14
-#endif
-#ifndef GLFW_JOYSTICK_1
-#define GLFW_JOYSTICK_1                     0
-#endif
-#ifndef GLFW_GAMEPAD_AXIS_LEFT_X
-#define GLFW_GAMEPAD_AXIS_LEFT_X            0
-#endif
-#ifndef GLFW_GAMEPAD_AXIS_LEFT_Y
-#define GLFW_GAMEPAD_AXIS_LEFT_Y            1
-#endif
-#ifndef GLFW_GAMEPAD_AXIS_RIGHT_X
-#define GLFW_GAMEPAD_AXIS_RIGHT_X           2
-#endif
-#ifndef GLFW_GAMEPAD_AXIS_RIGHT_Y
-#define GLFW_GAMEPAD_AXIS_RIGHT_Y           3
-#endif
-#ifndef GLFW_GAMEPAD_AXIS_LEFT_TRIGGER
-#define GLFW_GAMEPAD_AXIS_LEFT_TRIGGER      4
-#endif
-#ifndef GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER
-#define GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER     5
-#endif
-#ifndef GLFW_GAMEPAD_AXIS_LAST
-#define GLFW_GAMEPAD_AXIS_LAST              GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER
-#endif
+// Custom modification : Add support for older GLFW versions (<3.2) (on Rpi Stretch for example)
+#include "glfwCompatibilityHacks.h"
 
 // Data
 enum GlfwClientApi
