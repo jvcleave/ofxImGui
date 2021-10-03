26a9bc711d1eb1d763f001d4a38c8d022bf86dbf
manu home <manu@lit-labs.de>
2021-10-03 23:40:15 +0200

    added fag

--- src/LitImGuiHelper.h
+++ src/LitImGuiHelper.h
@@ -8,7 +8,11 @@ using namespace ImGui;
 
 namespace LitGui
 {
-	static IMGUI_API bool ImageButton(ImGuiID id, ImTextureID user_texture_id, const ImVec2& size, const ImVec2& uv0 = ImVec2(0, 0), const ImVec2& uv1 = ImVec2(1, 1), int frame_padding = -1, const ImVec4& bg_col = ImVec4(0, 0, 0, 0), const ImVec4& tint_col = ImVec4(1, 1, 1, 1))
+	static IMGUI_API bool ImageButton(ImGuiID id, ImTextureID user_texture_id, const ImVec2& size,
+	                                  ImGuiButtonFlags flags_p = 0, const ImVec2& uv0 = ImVec2(0, 0),
+	                                  const ImVec2& uv1 = ImVec2(1, 1), int frame_padding = -1,
+	                                  const ImVec4& bg_col = ImVec4(0, 0, 0, 0),
+	                                  const ImVec4& tint_col = ImVec4(1, 1, 1, 1))
 	{
 		ImGuiWindow* window = GetCurrentWindow();
 		if (window->SkipItems)
@@ -18,7 +22,9 @@ namespace LitGui
 		const ImGuiStyle& style = g.Style;
 
 
-		const ImVec2 padding = (frame_padding >= 0) ? ImVec2(static_cast<float>(frame_padding), static_cast<float>(frame_padding)) : style.FramePadding;
+		const ImVec2 padding = (frame_padding >= 0)
+			                       ? ImVec2(static_cast<float>(frame_padding), static_cast<float>(frame_padding))
+			                       : style.FramePadding;
 		const ImRect bb(window->DC.CursorPos, window->DC.CursorPos + size + padding * 2);
 		const ImRect image_bb(window->DC.CursorPos + padding, window->DC.CursorPos + padding + size);
 		ItemSize(bb);
@@ -26,10 +32,14 @@ namespace LitGui
 			return false;
 
 		bool hovered, held;
-		bool pressed = ButtonBehavior(bb, id, &hovered, &held);
+		bool pressed = ButtonBehavior(bb, id, &hovered, &held, flags_p);
 
 		// Render
-		const ImU32 col = GetColorU32((held && hovered) ? ImGuiCol_ButtonActive : hovered ? ImGuiCol_ButtonHovered : ImGuiCol_Button);
+		const ImU32 col = GetColorU32((held && hovered)
+			                              ? ImGuiCol_ButtonActive
+			                              : hovered
+			                              ? ImGuiCol_ButtonHovered
+			                              : ImGuiCol_Button);
 		RenderNavHighlight(bb, id);
 		RenderFrame(bb.Min, bb.Max, col, true, ImClamp(ImMin(padding.x, padding.y), 0.0f, style.FrameRounding));
 		if (bg_col.w > 0.0f)
